f31cd30a556dafa7356594450d8145ba05484ec3
mubashar81434 <mubasharsiddique81434@gmail.com>
2025-06-13 19:44:09 +0500

    adding index.php

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body style="background-color: black; color: white;">
+    <?php
+        echo "working";
+        echo "<br>";
+        class Car{
+            // class k ander computed values assign nhi kr skty 
+
+            // values
+            public $name="Toyota";
+            public $brand;
+            public $color;
+            
+            // methods
+            function showModel($number){
+                $this->model = $number;
+                echo "Model is $this->model";
+            }
+        }
+
+        $car1 = new Car; 
+
+        echo "$car1->name"."<br>";
+        $car1->showModel("al34");
+    ?>
+</body>
+</html>
