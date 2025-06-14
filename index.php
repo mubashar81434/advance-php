d7c59027dac4f2df7ca64896377a1bac75680509
mubashar81434 <mubasharsiddique81434@gmail.com>
2025-06-14 10:03:54 +0500

    adding the concept of inheritance

--- index.php
+++ index.php
@@ -69,12 +69,14 @@
     ?>
 
     <!-- Destructors -->
+
     <?php 
         // Jb hmy kisy object ko destory krna ho tb destructure ko call krty h 
         // jb hmry object ka scope khtm hota h tu destructure function automatically call hota h 
         // destructure name in php is __destruct
         // destructre ka koi b argumnet nhi hota 
 
+        /*
         class Student{
 
             function __construct($acceptParameter){
@@ -92,7 +94,49 @@
         }
 
         $student1 = new Student("Passed parameter");
+        */
+    ?>
+
+    <!-- Inheritance -->
+
+    <?php
+        // The mechanism of deriving a new class from old one is called inheritance 
+        // parent class ko base class b khty h 
+        // child class ko child ya extended class b khty h 
         
+        // types of inheritannce
+        // single, multi-level and hierarchical inheritance
+
+        // single inheritance
+
+        class Father {
+
+            public $a;
+
+            public $b;
+
+            function setValues($x, $y){
+                $this->a = $x;
+                $this->b = $y;
+            }
+
+        }
+
+        //  Son class extended from father 
+
+        class Son extends Father{
+
+            function display(){
+                echo "Value in A : $this->a <br>";
+                echo "Value in B : $this->b <br>";
+            }
+
+        }
+
+        $son1 = new Son; // new instance of Son class with the name son1
+        $son1->setValues(2,3); // method called from Father class because of inheritance 
+        $son1->display();
+
     ?>
 
 </body>
